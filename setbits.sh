260f379780b6b888f20449cbf3e66f805971754b
chaohan715 <chaohan715@gmail.com>
2018-11-25 22:41:08 -0500

    Create setbits.sh

---
+++ setbits.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+i=$1
+bit1=$(($i & 1))
+bit2=$((($i>>1) & 1))
+bit3=$((($i>>2) & 1))
+bit4=$((($i>>3) & 1))
+
+if test "$bit1" -eq 1
+then
+  gpio write 0 1
+fi
+
+if test "$bit2" -eq 1
+then
+  gpio write 1 1
+fi
+
+if test "$bit3" -eq 1
+then
+  gpio write 2 1
+fi
+
+if test "$bit4" -eq 1
+then
+  gpio write 3 1
+fi
